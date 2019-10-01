46ae24f0d917a5c288eda2a0bf17c01771a877c2
aruna.udayanga@layoutindex.lk <aruna.udayanga@layoutindex.lk>
2019-10-01 05:31:07 +0530

    Initial Commit

---
+++ ARKit-SCN-Download/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKit-SCN-Download
+//
+//  Created by LAYOUTindex on 10/1/19.
+//  Copyright © 2019 Aruna Udayanga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ARKit-SCN-DownloadTests/ARKit_SCN_DownloadTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARKit_SCN_DownloadTests.swift
+//  ARKit-SCN-DownloadTests
+//
+//  Created by LAYOUTindex on 10/1/19.
+//  Copyright © 2019 Aruna Udayanga. All rights reserved.
+//
+
+import XCTest
+@testable import ARKit_SCN_Download
+
+class ARKit_SCN_DownloadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
